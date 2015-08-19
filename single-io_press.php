f5065894e638ee1c2b6cc976cba313d5815e89a4
Adam Richards <arichard@nerdery.com>
2015-08-19 10:47:52 -0500

    updated press single page template to reflect comps

--- single-io_press.php
+++ single-io_press.php
@@ -6,116 +6,117 @@
  * @author arichard <arichard@nerdery.com>
  */
 
+get_header();
+
 ?>
 
-<div class="viewWindow-panel viewWindow-panel_feature">
-	<div class="viewWindow-panel-content">
-		<div class="viewWindow-panel-content-inner">
-			<div class="introBlock introBlock_fill">
-				<div class="introBlock-inner">
-					<div class="container">
-						<div class="topicBlock">
-							<div class="topicBlock-hd topicBlock-hd_plus">
-								<h2 class="hdg hdg_2 mix-hdg_bold"><?php the_title(); ?></h2>
-							</div>
-							<div class="topicBlock-subHd">
-								<div class="hdg hdg_6 mix-hdg_italic mix-hdg_gray"><?php internetorg_posted_on(); ?></div>
-							</div>
-							<div class="topicBlock-bd">
-								<p class="bdcpy">
-									<?php the_content(); ?>
-								</p>
+<div class="viewWindow isShifted js-stateDefault">
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php
+
+		/**
+		 * @todo replace this panel when real content is available
+		 *
+		 * @note this is just a dummy panel used for positioning
+		 *
+		 */
+
+		?>
+
+	<div class="viewWindow-panel">
+		<div class="viewWindow-panel-content">
+			<div class="viewWindow-panel-content-inner">
+				<div class="introBlock introBlock_fill">
+					<div class="introBlock-inner">
+						<div class="container">
+							<div class="topicBlock">
+								<div class="topicBlock-hd topicBlock-hd_plus">
+									<h2 class="hdg hdg_3 mix-hdg_bold">Example Intro Block</h2>
+								</div>
+								<div class="topicBlock-subHd"><div class="hdg hdg_5 mix-hdg_italic mix-hdg_gray">May 25, 2015</div></div>
+								<div class="topicBlock-bd">
+									<p class="bdcpy">Will need to replace this layout with the home page narrative one.</p>
+								</div>
+								<div class="topicBlock-cta"><a class="btn js-stateLink" href="/approach" data-type="panel" data-title="Our Approach" data-image="http://placehold.it/400x800?text=APPROACH" data-theme="Approach">Our Approach</a></div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="introBlock-ft introBlock-ft_rule">
-					<ul class="socialParade">
-						<li><a class="socialParade-icon socialParade-icon_fb" href=""><?php esc_html_e( 'Facebook', 'internetorg' ); ?></a></li>
-						<li><a class="socialParade-icon socialParade-icon_tw" href=""><?php esc_html_e( 'Twitter', 'internetorg' ); ?></a></li>
-						<li><a class="socialParade-icon socialParade-icon_li" href=""><?php esc_html_e( 'LinkedIn', 'internetorg' ); ?></a></li>
-					</ul>
+					<div class="introBlock-ft introBlock-ft_rule">
+						<ul class="socialParade">
+							<li><a class="socialParade-icon socialParade-icon_fb" href="">Facebook</a></li>
+							<li><a class="socialParade-icon socialParade-icon_tw" href="">Twitter</a></li>
+							<li><a class="socialParade-icon socialParade-icon_li" href="">LinkedIn</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-
-<div class="viewWindow-panel viewWindow-panel_story">
-	<div class="viewWindow-panel-content">
-		<div class="viewWindow-panel-content-inner">
-			<?php /* Article Content */ ?>
-			<?php the_content(); ?>
-
-			<?php /* Free Services */ ?>
-			<div class="contentCol">
-				<div class="container">
-					<div class="feature">
-						<div class="feature-hd">
-							<h2 class="hdg hdg_3"><?php esc_html__( 'Full List of Free Services', 'internetorg' ); ?></h2>
-						</div>
-							<div class="feature-bd">
-								<ul class="servicesList">
-								<?php $freeServices = internetorg_get_free_services(); ?>
-								<?php if ( ! empty( $freeServices )  ) : ?>
-									<?php foreach ( $freeServices as $service ) : ?>
-										<li>
-											<a href="">
-												<div class="servicesList-item">
-													<?php if ( ! empty( $service['image'] ) ) : ?>
-													<div class="servicesList-item-icon">
-														<img src="<?php echo esc_attr( $service['image'] ); ?>" alt="<?php echo esc_attr( $service['title'] ); ?>" />
-													</div>
-													<?php endif; ?>
-													<div class="servicesList-item-bd">
-														<div class="hdg hdg_5"><?php echo esc_html( $service['title'] ); ?></div>
-														<div class="bdcpy bdcpy_sm"><?php echo esc_html( $service['excerpt'] ); ?></div>
-													</div>
-												</div>
-											</a>
-										</li>
-									<?php endforeach; ?>
-								<?php endif; ?>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<?php /* Related Articles */ ?>
-			<div class="footBox">
-				<div class="container">
-					<div class="footBox-hd"><?php esc_html_e( 'More Posts', 'internetorg' ) ?></div>
-					<div class="vList vList_footBox">
-						<div>
+	<div class="viewWindow-panel viewWindow-panel_feature">
+		<div class="viewWindow-panel-content">
+			<div class="viewWindow-panel-content-inner">
+				<div class="introBlock introBlock_fill">
+					<div class="introBlock-inner">
+						<div class="container">
 							<div class="topicBlock">
-								<div class="topicBlock-hd">
-									<h2 class="hdg hdg_8 mix-hdg_bold">Max Three Lines Article Title on Mobile Ipsum</h2>
+
+								<div class="topicBlock-hd topicBlock-hd_plus">
+									<h2 class="hdg hdg_2 mix-hdg_bold"><?php the_title(); ?></h2>
+								</div>
+								<div class="topicBlock-subHd">
+									<div class="hdg hdg_6 mix-hdg_italic mix-hdg_gray"><?php internetorg_posted_on_date(); ?></div>
 								</div>
 								<div class="topicBlock-bd">
 									<p class="bdcpy">
-									Fast facts about Internet.org and free basic services lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										<?php echo sanitize_text_field( internetorg_get_the_subtitle( get_the_ID() ) ); ?>
 									</p>
 								</div>
-								<div class="topicBlock-cta"><a href="" class="btn">Read</a></div>
-							</div>
-						</div>
-						<div>
-							<div class="topicBlock">
-								<div class="topicBlock-hd">
-									<h2 class="hdg hdg_8 mix-hdg_bold">Article Title</h2>
-								</div>
-								<div class="topicBlock-bd"><p class="bdcpy">Example of very short copy.</p></div>
-								<div class="topicBlock-cta"><a href="" class="btn">Read</a></div>
+
 							</div>
 						</div>
 					</div>
+					<div class="introBlock-ft introBlock-ft_rule">
+						<?php
+						/**
+						 * @todo replace with correct social media links/configs
+						 */ ?>
+						<ul class="socialParade">
+							<li><a class="socialParade-icon socialParade-icon_fb" href=""><?php esc_html_e( 'Facebook', 'internetorg' ); ?></a></li>
+							<li><a class="socialParade-icon socialParade-icon_tw" href=""><?php esc_html_e( 'Twitter', 'internetorg' ); ?></a></li>
+							<li><a class="socialParade-icon socialParade-icon_li" href=""><?php esc_html_e( 'LinkedIn', 'internetorg' ); ?></a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
-
-
 		</div>
 	</div>
+
+	<div class="viewWindow-panel viewWindow-panel_story isActive">
+		<div class="viewWindow-panel-content">
+			<div class="viewWindow-panel-content-inner">
+				<div>
+
+					<div class="contentCol">
+						<div class="container">
+							<div class="feature">
+								<div class="feature-bd wysiwyg quarantine">
+									<?php the_content(); ?>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endwhile; ?>
 </div>
 
+<?php
+
+// get_sidebar();
+get_footer();
