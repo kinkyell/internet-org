2214b8c001ff838c4867284c3ca6ff798dbb611b
Jason Dicks <jdicks@nerdery.com>
2015-08-24 16:49:52 -0500

    addressed scroll sensitivity issue

--- _static/src/assets/scripts/appConfig.js
+++ _static/src/assets/scripts/appConfig.js
@@ -47,7 +47,7 @@ define(function(require, exports, module) { // jshint ignore:line
                 EASE: window.Expo, // gsap ease type
                 EASE_DIRECTION_FORWARD: 'easeOut', // gsap ease direction when progressing forward
                 EASE_DIRECTION_REVERSE: 'easeIn', // gsap ease direction when progressing backward
-                SCROLL_BUFFER: 0, // Buffer for scroll jacking (ms)
+                SCROLL_BUFFER: 250, // Buffer for scroll jacking (ms)
                 TIME_SCALE: 0.5, // Timeline speed multiplier
                 SECTION_DURATION: 0.35, // uniform transformBlock section durations
                 featureImages: { // Featured images
