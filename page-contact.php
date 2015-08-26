f0cac39d17c71dd1194ce94d3f4c1caf2a725926
Nick Stark <nstark@nerdery.com>
2015-08-26 16:45:49 -0500

    added wrapper to make async contact page work

--- page-contact.php
+++ page-contact.php
@@ -43,81 +43,83 @@ get_header();
 
 	<div id="storyPanel" class="viewWindow-panel viewWindow-panel_story isActive">
 		<div class="viewWindow-panel-content">
+			<div class="viewWindow-panel-content-inner">
 
-			<?php
-			$custom_fields = get_post_meta( get_the_ID(), 'home-content-section', false );
-			if ( ! empty( $custom_fields ) ) :
-				foreach ( $custom_fields as $group ) : ?>
-					<div class="contentCol contentCol_divided">
-						<div class="container">
-					<?php if ( ! empty( $group ) ) :
-						foreach ( $group as $fieldset ) : ?>
-
-					<div class="feature">
-						<?php if ( ! empty( $fieldset['title'] ) ) : ?>
-						<div class="feature-hd">
-							<div class="hdg hdg_4"><?php echo esc_html( $fieldset['title'] ); ?></div>
-						</div>
-						<?php endif; ?>
-
-						<?php if ( ! empty( $fieldset['content'] ) ) : ?>
-						<div class="feature-bd">
-							<p class="bdcpy">
-								<?php echo esc_html( strip_tags( $fieldset['content'] ) ); ?>
-							</p>
-						</div>
-						<?php endif; ?>
-
-						<?php if ( ! empty( $fieldset['call-to-action'] ) ) : ?>
-							<?php foreach ( $fieldset['call-to-action'] as $cta ) : ?>
-								<div class="feaure-cta">
-								<?php if ( ! empty( $cta['link'] ) ) : ?>
-									<a href="<?php echo esc_attr( $cta['link'] ); ?>" class="link"><?php echo esc_html__( 'Learn More', 'internetorg' ); ?></a>
-								<?php endif; ?>
-								</div>
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</div>
 				<?php
-						endforeach;
-					endif;
-				?>
-				</div>
-			</div>
+				$custom_fields = get_post_meta( get_the_ID(), 'home-content-section', false );
+				if ( ! empty( $custom_fields ) ) :
+					foreach ( $custom_fields as $group ) : ?>
+						<div class="contentCol contentCol_divided">
+							<div class="container">
+						<?php if ( ! empty( $group ) ) :
+							foreach ( $group as $fieldset ) : ?>
 
-			<?php
-				endforeach;
-			endif; ?>
-
-			<div class="container">
-				<div class="contentCol">
-					<div class="vr vr_x1">
-						<div class="hdg hdg_3"><?php echo esc_html__( 'Get in Touch', 'internetorg' ); ?></div>
-					</div>
-
-					<div class="vendorForm">
-					<?php the_content(); ?>
-					</div>
-
-				</div><!-- /.contentCol -->
-			</div>
-
-			<div class="socialBlock">
-				<div class="socialBlock-inner">
-					<div class="container">
-						<div class="fbFollowBlock">
-							<div class="fbFollowBlock-bd">
-								<h2 class="hdg hdg_3 mix-hdg_white"><?php echo esc_html__( 'Follow the Project', 'internetorg' ); ?></h2>
-								<p class="bdcpy mix-bdcpy_light"><?php echo esc_html__( 'Stay updated about Internet.org and lorem ipsum dolor sit amet.', 'internetorg' ); ?></p>
+						<div class="feature">
+							<?php if ( ! empty( $fieldset['title'] ) ) : ?>
+							<div class="feature-hd">
+								<div class="hdg hdg_4"><?php echo esc_html( $fieldset['title'] ); ?></div>
 							</div>
-							<div class="fbFollowBlock-cta">
-								<a href="<?php echo esc_attr__( 'https://fb.me/Internetdotorg', 'internetorg' ); ?>" class="btn btn_facebook"><?php echo esc_html__( 'Like us on Facebook', 'internetorg' ); ?></a>
+							<?php endif; ?>
+
+							<?php if ( ! empty( $fieldset['content'] ) ) : ?>
+							<div class="feature-bd">
+								<p class="bdcpy">
+									<?php echo esc_html( strip_tags( $fieldset['content'] ) ); ?>
+								</p>
+							</div>
+							<?php endif; ?>
+
+							<?php if ( ! empty( $fieldset['call-to-action'] ) ) : ?>
+								<?php foreach ( $fieldset['call-to-action'] as $cta ) : ?>
+									<div class="feaure-cta">
+									<?php if ( ! empty( $cta['link'] ) ) : ?>
+										<a href="<?php echo esc_attr( $cta['link'] ); ?>" class="link"><?php echo esc_html__( 'Learn More', 'internetorg' ); ?></a>
+									<?php endif; ?>
+									</div>
+								<?php endforeach; ?>
+							<?php endif; ?>
+						</div>
+					<?php
+							endforeach;
+						endif;
+					?>
+					</div>
+				</div>
+
+				<?php
+					endforeach;
+				endif; ?>
+
+				<div class="container">
+					<div class="contentCol">
+						<div class="vr vr_x1">
+							<div class="hdg hdg_3"><?php echo esc_html__( 'Get in Touch', 'internetorg' ); ?></div>
+						</div>
+
+						<div class="vendorForm">
+						<?php the_content(); ?>
+						</div>
+
+					</div><!-- /.contentCol -->
+				</div>
+
+				<div class="socialBlock">
+					<div class="socialBlock-inner">
+						<div class="container">
+							<div class="fbFollowBlock">
+								<div class="fbFollowBlock-bd">
+									<h2 class="hdg hdg_3 mix-hdg_white"><?php echo esc_html__( 'Follow the Project', 'internetorg' ); ?></h2>
+									<p class="bdcpy mix-bdcpy_light"><?php echo esc_html__( 'Stay updated about Internet.org and lorem ipsum dolor sit amet.', 'internetorg' ); ?></p>
+								</div>
+								<div class="fbFollowBlock-cta">
+									<a href="<?php echo esc_attr__( 'https://fb.me/Internetdotorg', 'internetorg' ); ?>" class="btn btn_facebook"><?php echo esc_html__( 'Like us on Facebook', 'internetorg' ); ?></a>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
+			</div>
 		</div>
 	</div>
 
