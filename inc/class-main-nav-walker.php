fd5a8df82ecf067051fea0455d2893aa045d7da0
Richard Aber <raber@nerdery.com>
2015-08-27 16:48:08 -0500

    comment style updates per VIP Scanner

--- inc/class-main-nav-walker.php
+++ inc/class-main-nav-walker.php
@@ -1,41 +1,41 @@
 <?php
 /**
- * This class extends the default nav menu walker for the primary nav piece
- * of the main menu
+ * This class extends the default nav menu walker for the primary nav piece of the main menu.
  *
  * @package Internet.org
- * @author arichard <arichard@nerdery.com>
+ * @author  arichard <arichard@nerdery.com>
  */
 
 /**
- * Class Internetorg_Main_Nav_Walker used to use our own html with the menu
+ * Class Internetorg_Main_Nav_Walker used to use our own html with the menu.
  *
  * @package Internet.org
- * @author arichard <arichard@nerdery.com>
+ * @author  arichard <arichard@nerdery.com>
  */
-class Internetorg_Main_Nav_Walker extends Walker_Nav_Menu
-{
+class Internetorg_Main_Nav_Walker extends Walker_Nav_Menu {
 	/**
-	 * display content to start the menu
+	 * Display content to start the menu.
 	 *
-	 * @param string $output html to be output - passed by reference
-	 * @param int $depth nesting depth of this menu
-	 * @param array $args configuration args for this menu
+	 * @param string $output Markup to be output - passed by reference.
+	 * @param int    $depth  Nesting depth of this menu.
+	 * @param array  $args   Configuration args for this menu.
+	 *
 	 * @return void
 	 */
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' );
+		$indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' );
 		$output .= "\n" . $indent . '<ul>' . "\n";
 	}
 
 	/**
-	 * build the starting element for this menu item
+	 * Build the starting element for this menu item.
 	 *
-	 * @param string $output html to be output - passed by reference
-	 * @param object $item current item data
-	 * @param int $depth what level is this item
-	 * @param array $args arguments used to configure this item
-	 * @param int $id menu item id
+	 * @param string $output Markup to be output - passed by reference.
+	 * @param object $item   Current item data.
+	 * @param int    $depth  What level is this item.
+	 * @param array  $args   Arguments used to configure this item.
+	 * @param int    $id     Menu item id.
+	 *
 	 * @return void
 	 */
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
@@ -44,11 +44,11 @@ class Internetorg_Main_Nav_Walker extends Walker_Nav_Menu
 
 		$output .= $indent . '<li>';
 
-		// link attributes
-		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) . '"' : '';
-		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target )     . '"' : '';
-		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn )        . '"' : '';
-		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url )        . '"' : '';
+		// Link attributes.
+		$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
 		$attributes .= ' ';
 		if ( internetorg_is_internal_url( $item->url ) ) {
 			$attributes .= ' class="topicLink-link js-stateLink"';
@@ -66,7 +66,8 @@ class Internetorg_Main_Nav_Walker extends Walker_Nav_Menu
 			$args = (object) $args;
 		}
 
-		$item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+		$item_output = sprintf(
+			'%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
 			$args->before,
 			$attributes,
 			$args->link_before,
@@ -75,11 +76,12 @@ class Internetorg_Main_Nav_Walker extends Walker_Nav_Menu
 			$args->after
 		);
 
-		$customDiv = '<div class="topicLink topicLink_theme' . esc_attr( substr( $item->title, 4 ) ) . ' js-menuView-slider" style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">';
+		$customDiv = '<div class="topicLink topicLink_theme' . esc_attr( substr( $item->title, 4 ) )
+		             . ' js-menuView-slider" style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">';
+
 		$item_output = $customDiv . $item_output . '</div>';
 
-		// build html
+		// Build the HTML markup.
 		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
-
 }
