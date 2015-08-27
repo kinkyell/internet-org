522695bc6a794b243ed8aa2e1a21c06290b8eb34
Richard Aber <raber@nerdery.com>
2015-08-27 14:49:48 -0500

    remove line that required custom header file

--- functions.php
+++ functions.php
@@ -286,11 +286,6 @@ endif;
 require get_template_directory() . '/inc/enqueue-scripts.php';
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
