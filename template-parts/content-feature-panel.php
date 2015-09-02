f374796bda2b9090214c52be1d8f23ffb8e13fc0
Richard Aber <raber@nerdery.com>
2015-09-01 19:19:32 -0500

    use panel-image registered image size for the data-image att in content feature panel and next page template parts

--- template-parts/content-feature-panel.php
+++ template-parts/content-feature-panel.php
@@ -14,7 +14,7 @@ $id = get_the_ID();
 		<?php if ( empty( $id ) ) : ?>
 			<div class="viewWindow-panel-content-inner" style="background-image: url(<?php echo esc_url( get_stylesheet_directory_uri() . '/_static/web/assets/media/uploads/home.jpg' ); ?>);"></div>
 		<?php else : ?>
-			<div class="viewWindow-panel-content-inner" style="background-image: url(<?php echo esc_url( internetorg_get_post_thumbnail( get_the_ID() ) ); ?>);"></div>
+			<div class="viewWindow-panel-content-inner" style="background-image: url(<?php echo esc_url( internetorg_get_post_thumbnail( get_the_ID(), 'panel-image' ) ); ?>);"></div>
 		<?php endif; ?>
 	</div>
 </div><!-- end viewWindow-panel_feature -->
