e6e9d6ce9ba5b117cf66367125b06dd4d7a1ec2d
Richard Aber <raber@nerdery.com>
2015-09-03 14:51:57 -0500

    update file docblock and excerpt approach

--- template-parts/content-single-more.php
+++ template-parts/content-single-more.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: raber
- * Date: 9/3/15
- * Time: 1:22 PM
+ * Content Single More posts template part.
+ *
+ * @package Internet.org
  */
-
 
 $next_post = get_next_post();
 $prev_post = get_previous_post();
@@ -16,7 +14,10 @@ if ( empty( $next_post ) && empty( $prev_post ) ) {
 
 ?>
 
-<div class="footBox-hd"><?php esc_html_e( 'More Posts', 'internetorg' ) ?></div>
+<div class="footBox-hd">
+	<?php esc_html_e( 'More Posts', 'internetorg' ) ?>
+</div>
+
 <div class="vList vList_footBox">
 
 	<?php if ( ! empty( $next_post ) ) : ?>
@@ -33,7 +34,9 @@ if ( empty( $next_post ) && empty( $prev_post ) ) {
 					</p>
 				</div>
 				<div class="topicBlock-cta">
-					<a class="btn js-stateLink" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>" data-title="<?php echo apply_filters( 'the_title', esc_attr( $next_post->post_title ) ); ?>" data-desc="<?php echo esc_attr( get_the_date( '', $next_post->ID ) ); ?>" data-type="titled">
+					<a class="btn js-stateLink" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>"
+					   data-title="<?php echo apply_filters( 'the_title', esc_attr( $next_post->post_title ) ); ?>"
+					   data-desc="<?php echo esc_attr( get_the_date( '', $next_post->ID ) ); ?>" data-type="titled">
 						<?php esc_html_e( 'Read', 'internetorg' ); ?>
 					</a>
 				</div>
@@ -55,7 +58,9 @@ if ( empty( $next_post ) && empty( $prev_post ) ) {
 					</p>
 				</div>
 				<div class="topicBlock-cta">
-					<a class="btn js-stateLink" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>" data-title="<?php echo apply_filters( 'the_title', esc_attr( $prev_post->post_title ) ); ?>" data-desc="<?php echo esc_attr( get_the_date( '', $prev_post->ID ) ); ?>" data-type="titled">
+					<a class="btn js-stateLink" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>"
+					   data-title="<?php echo apply_filters( 'the_title', esc_attr( $prev_post->post_title ) ); ?>"
+					   data-desc="<?php echo esc_attr( get_the_date( '', $prev_post->ID ) ); ?>" data-type="titled">
 						<?php esc_html_e( 'Read', 'internetorg' ); ?>
 					</a>
 				</div>
