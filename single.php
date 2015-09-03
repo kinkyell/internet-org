81d545e4ddd8ac455c22739cd1f041aa7cf9569f
Nick Stark <nstark@nerdery.com>
2015-09-03 11:49:59 -0500

    added metadata to single templates FIIORG-144

--- single.php
+++ single.php
@@ -9,7 +9,7 @@ get_header();
 
 ?>
 
-	<div class="viewWindow isShifted js-viewWindow js-stateDefault" data-type="titled">
+	<div class="viewWindow isShifted js-viewWindow js-stateDefault" data-type="titled" data-route="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>" data-desc="<?php echo esc_html( internetorg_get_the_subtitle( get_the_ID() ) ); ?>" data-date="<?php internetorg_posted_on_date(); ?>">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'page-temp-panel' ); ?>
