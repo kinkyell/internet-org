4633f0cb7d18e703fcc751fce1e6d17c4f34ba9b
Richard Aber <raber@nerdery.com>
2015-09-18 16:10:22 -0500

    Remove title shim, no longer necessary with WP 4.3+

--- inc/extras.php
+++ inc/extras.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates
+ * Custom functions that act independently of the theme templates.
  *
- * Eventually, some of the functionality here could be replaced by core features
+ * Eventually, some of the functionality here could be replaced by core features.
  *
  * @package Internet.org
  */
@@ -25,63 +25,9 @@ function internetorg_body_classes( $classes ) {
 
 add_filter( 'body_class', 'internetorg_body_classes' );
 
-if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @param string $title Default title text for current view.
-	 * @param string $sep   Optional separator.
-	 *
-	 * @return string The filtered title.
-	 */
-	function internetorg_wp_title( $title, $sep ) {
-		if ( is_feed() ) {
-			return $title;
-		}
-
-		global $page, $paged;
-
-		// Add the blog name.
-		$title .= esc_html( get_bloginfo( 'name', 'display' ) );
-
-		// escape $sep
-		$sep = esc_html( $sep );
-
-		// Add the blog description for the home/front page.
-		$site_description = esc_html( get_bloginfo( 'description', 'display' ) );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= sprintf( ' %s %s', $sep, $site_description );
-		}
-
-		// Add a page number if necessary.
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= sprintf( ' %s ' . esc_html__( 'Page %s', 'internetorg' ), $sep, max( $paged, $page ) );
-		}
-
-		return $title;
-	}
-
-	add_filter( 'wp_title', 'internetorg_wp_title', 10, 2 );
-
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function internetorg_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
-	}
-
-	add_action( 'wp_head', 'internetorg_render_title' );
-endif;
-
-
 if ( ! function_exists( 'internetorg_change_excerpt_length' ) ) :
 	/**
-	 * Change the default length of the excerpts to 25 words (from default 55)
+	 * Change the default length of the excerpts to 25 words (from default 55).
 	 *
 	 * @param int $length Original length.
 	 *
@@ -96,7 +42,7 @@ endif;
 
 if ( ! function_exists( 'internetorg_change_excerpt_more' ) ) :
 	/**
-	 * Change the default "more" indicator
+	 * Change the default "more" indicator.
 	 *
 	 * @param string $more Current more indicator.
 	 *
