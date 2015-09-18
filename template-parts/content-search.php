4735ec704d3a7da241e7f1963609004056d60803
Michael Sawicki <msawicki@nerdery.com>
2015-09-18 17:11:58 -0400

    adds strict operator

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -14,7 +14,7 @@ $img = ( internetorg_get_media_image_url( get_post_thumbnail_id( get_the_ID() ),
 	?  internetorg_get_media_image_url( get_post_thumbnail_id( get_the_ID() ), 'panel-image' )
 	: '';
 $mobile_image = esc_url( internetorg_get_mobile_featured_image( get_post_type( get_the_ID() ), get_the_ID() ) );
-$theme = ( get_post_type( get_the_ID() ) == 'io_story') ? 'approach' : '';
+$theme = ( get_post_type( get_the_ID() ) === 'io_story') ? 'approach' : '';
 ?>
 
 <div class="resultsList-list-item">
