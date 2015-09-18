09fd4cbab6b6325293231ff15fe74146a5a9a3a3
Michael Sawicki <msawicki@nerdery.com>
2015-09-18 16:27:43 -0400

    fixes escaping errors

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -27,7 +27,7 @@ $theme = ( get_post_type( get_the_ID() ) == 'io_story') ? 'approach' : '';
 		</div>
 		<div class="feature-cta">
 			<a href="<?php the_permalink(); ?>" class="link mix-link_small js-stateLink"
-			    data-title="<?php echo apply_filters( 'the_title', esc_attr( $post->post_title ) ); ?>"
+			    data-title="<?php echo esc_attr(apply_filters( 'the_title',  $post->post_title  ) ); ?>"
 			    data-desc="<?php echo wp_kses_post( get_post_field( 'post_excerpt', get_the_ID() ) ); ?>"
 			    data-image="<?php echo esc_url( $img );?>"
 			    data-mobile-image="<?php echo esc_url( $mobile_image );?>"
@@ -36,7 +36,7 @@ $theme = ( get_post_type( get_the_ID() ) == 'io_story') ? 'approach' : '';
 				    data-date="<?php echo esc_attr( get_the_date( '', $post->ID ) ); ?>"
 				    data-social="true"
 			    <?php } ?>
-			   data-type="<?php echo $type; ?>">
+			   data-type="<?php echo esc_attr( $type ); ?>">
 				<?php esc_html_e( 'Read More', 'internetorg' ); ?>
 			</a>
 		</div>
