56a38290c1fba27821ea502e12cf0ec981643b4b
Richard Aber <raber@nerdery.com>
2015-09-25 15:31:55 -0500

    Do not output page intro mobile markup if fields are empty

--- template-parts/content-page-intro-mobile.php
+++ template-parts/content-page-intro-mobile.php
@@ -5,15 +5,52 @@
  * @package Internet.org
  */
 
+/**
+ * The mobile featured image URL.
+ *
+ * @var string $mobile_featured_image
+ */
+$mobile_featured_image = internetorg_get_mobile_featured_image( get_post_type(), get_the_ID() );
+
+/**
+ * The intro_title meta.
+ *
+ * @var string $mobile_intro_title
+ */
+$mobile_intro_title = internetorg_get_the_intro_block( get_the_ID(), 'intro_title' );
+
+/**
+ * The intro_content meta.
+ *
+ * @var string $mobile_intro_content
+ */
+$mobile_intro_content = internetorg_get_the_intro_block( get_the_ID(), 'intro_content' );
+
+if ( empty( $mobile_featured_image ) && empty( $mobile_intro_title ) && empty( $mobile_intro_content ) ) {
+	return;
+}
+
 ?>
 
-<!-- Secondary (mobile) Feature Image -->
-<div class="topicBlock-media isHidden u-isHiddenMedium" aria-hidden="true">
-	<img src="<?php echo esc_url( internetorg_get_mobile_featured_image( get_post_type(), get_the_ID() ) ); ?>" alt="" />
-</div>
+<?php if ( ! empty( $mobile_featured_image ) ) : ?>
+	<!-- Secondary (mobile) Feature Image -->
+	<div class="topicBlock-media isHidden u-isHiddenMedium" aria-hidden="true">
+		<img src="<?php echo esc_url( $mobile_featured_image ); ?>" alt="" />
+	</div>
+<?php endif; ?>
+
+<?php
+if ( empty( $mobile_intro_title ) && empty( $mobile_intro_content ) ) {
+	return;
+}
+?>
 
 <!-- Duplicate Content - Mobile Only -->
 <div class="topicBlock-bd isHidden u-isHiddenMedium" aria-hidden="true">
-	<div class="hdg hdg_3"><?php echo esc_html( internetorg_get_the_intro_block( get_the_ID(), 'intro_title' ) ); ?></div>
-	<p class="bdcpy"><?php echo esc_html( internetorg_get_the_intro_block( get_the_ID(), 'intro_content' ) ); ?></p>
+	<?php if ( ! empty( $mobile_intro_title ) ) : ?>
+		<div class="hdg hdg_3"><?php echo esc_html( $mobile_intro_title ); ?></div>
+	<?php endif; ?>
+	<?php if ( ! empty( $mobile_intro_content ) ) : ?>
+		<p class="bdcpy"><?php echo esc_html( $mobile_intro_content ); ?></p>
+	<?php endif; ?>
 </div>
