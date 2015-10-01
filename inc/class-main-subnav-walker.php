cc21b4dd32231b11d687225ca21982d7d5cf3a57
Richard Aber <raber@nerdery.com>
2015-10-01 13:58:10 -0500

    Account for paneled content in subnav

--- inc/class-main-subnav-walker.php
+++ inc/class-main-subnav-walker.php
@@ -44,18 +44,132 @@ class Internetorg_Main_SubNav_Walker extends Walker_Nav_Menu {
 
 		$output .= $indent . '<li>';
 
+		/**
+		 * Custom offsite links should not have js-stateLink applied to them.
+		 */
+		if ( 'custom' === $item->type && ! internetorg_is_internal_url( $item->url ) ) {
+			/**
+			 * The classes to apply to the link.
+			 *
+			 * @var string $class
+			 */
+			$class = 'auxLink';
+		} else {
+			$class = 'auxLink js-stateLink';
+		}
+
+		/**
+		 * Get the title and description of the object if it's a post of any post_type.
+		 */
+		if ( 'post_type' === $item->type ) {
+
+			/**
+			 * Title of the post.
+			 *
+			 * @var string $data_title
+			 */
+			$data_title = get_the_title( $item->object_id );
+
+			/**
+			 * Subtitle meta or post_excerpt for description on the link.
+			 *
+			 * @var string $data_desc
+			 */
+			$data_desc  = internetorg_get_the_subtitle( $item->object_id );
+
+			if ( empty( $data_desc ) ) {
+				$data_desc = get_post_field( 'post_excerpt', $item->object_id );
+			}
+		}
+
+		/**
+		 * The page and io_story post types are the only ones that should be paneled.
+		 *
+		 * But only if we have a post_thumbnail, and not for blog home or page-contact.php.
+		 */
+		if ( 'io_story' === $item->object || 'page' === $item->object ) {
+
+			/**
+			 * The page for posts setting is the blog home.
+			 *
+			 * @var int $blog_home
+			 */
+			$blog_home = get_option( 'page_for_posts' );
+
+			/**
+			 * Page template filename, empty when default template is in use, false if post is not a page.
+			 *
+			 * @var string|bool $template_slug
+			 */
+			$template_slug = get_page_template_slug( $item->object_id );
+
+			/**
+			 * True or false. Do we have a post_thumbnail or not.
+			 *
+			 * @var bool $has_post_thumbnail
+			 */
+			$has_post_thumbnail = has_post_thumbnail( $item->object_id );
+
+			if ( $has_post_thumbnail && $item->object_id !== $blog_home && 'page-contact.php' !== $template_slug ) {
+
+				/**
+				 * The data-type attribute for the link, indicates if this is a paneled or titled destination link.
+				 * @var string $data_type
+				 */
+				$data_type = 'panel';
+
+				/**
+				 * URL to the post_thumbnail of panel-image size.
+				 *
+				 * @var string $post_thumbnail
+				 */
+				$post_thumbnail = internetorg_get_post_thumbnail(
+					$item->object_id,
+					'panel-image'
+				);
+
+				/**
+				 * URL to the mobile post_thumbnail of inline-image size.
+				 *
+				 * @var string $mobile_thumbnail
+				 */
+				$mobile_thumbnail = internetorg_get_mobile_featured_image(
+					get_post_type( $item->object_id ),
+					$item->object_id
+				);
+			}
+		}
+
+		if ( 'post' === $item->object ) {
+			/**
+			 * Posts have a date attribute for publish date.
+			 *
+			 * @var string $data_date
+			 */
+			$data_date = get_the_date( '', $item->object_id );
+			/**
+			 * Posts have a social attribute.
+			 *
+			 * @var string $data_social
+			 */
+			$data_social = 'true';
+		}
+
 		// Link attributes.
-		$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes = ! empty( $class ) ? ' class="' . esc_attr( $class ) . '"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_url( $item->url ) . '"' : '';
+		$attributes .= ! empty( $data_title )
+			? ' data-title="' . esc_attr( $data_title ) . '"'
+			: ' data-title="' . esc_attr( $item->title ) . '"';
+		$attributes .= ! empty( $data_desc ) ? ' data-desc="' . esc_attr( $data_desc ) . '"' : '';
+		$attributes .= ! empty( $data_date ) ? ' data-date="' . esc_attr( $data_date ) . '"' : '';
+		$attributes .= ! empty( $post_thumbnail ) ? ' data-image="' . esc_url( $post_thumbnail ) . '"' : '';
+		$attributes .= ! empty( $mobile_thumbnail ) ? ' data-mobile-image="' . esc_url( $mobile_thumbnail ) . '"' : '';
+		$attributes .= ! empty( $data_social ) ? ' data-social="' . esc_attr( $data_social ) . '"' : '';
+		$attributes .= ! empty( $data_type ) ? ' data-type="' . esc_attr( $data_type ) . '"' : ' data-type="titled"';
+		$attributes .= ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
 		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
 		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
-		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
-		$attributes .= ' ';
-
-		if ( internetorg_is_internal_url( $item->url ) ) {
-			$attributes .= ' class="auxLink js-stateLink" data-type="titled" data-title="' . esc_attr( $item->title ) . '"';
-		} else {
-			$attributes .= ' class="auxLink"';
-		}
 
 		if ( is_array( $args ) && ! empty( $args ) ) {
 			$args = (object) $args;
