8ea414139d2dd7cac964991805b668447708c578
Ben Koren <bkoren@nerdery.com>
2015-10-20 13:26:55 -0500

    Added preliminary DOMDocument parser.

--- plugins/internetorg-link-filter/includes/ContentParser.class.php
+++ plugins/internetorg-link-filter/includes/ContentParser.class.php
@@ -28,8 +28,52 @@ class ContentParser {
    */
   public function parseLinks($content) {
 
-    // @TODO: parse through DOM, for each link: $newLink = $this->linkTransformer($oldLink);
+    if (empty($content)) {
+      return $content;
+    }
 
-    return $content;
+    $dom = new DOMDocument();
+    $dom->loadHTML($content);
+
+    // If the content could not be parsed as HTML, do not filter.
+    if (!$dom) {
+      return $content;
+    }
+
+    // Recurse through the content, transforming all applicable links
+    $this->traverseDom($dom, $dom);
+
+    $transformedContent = $dom->saveHTML();
+
+    return $transformedContent;
+  }
+
+  /**
+   * Recursive method used to traverse the DOM and filter all links.
+   *
+   * @param DOMDocument $dom
+   * @param DomNode $node
+   */
+  protected function traverseDom(DOMDocument $dom, DomNode &$node) {
+    if ($node->childNodes) {
+      for ($i = 0; $i < $node->childNodes->length; ++$i) {
+        /**
+         * @var DomElement
+         */
+        $childNode = $node->childNodes->item($i);
+        if ($childNode->tagName == 'a') {
+          $href = $childNode->getAttribute('href');j
+
+          // @TODO: pass href to LinkTransformer for transformation.
+
+//          $textReplacementNode = $dom->createTextNode($childNode->data);
+//          $node->replaceChild(
+//            $textReplacementNode,
+//            $childNode
+//          );
+        }
+        $this->traverseDom($dom, $node->childNodes->item($i));
+      }
+    }
   }
 }
