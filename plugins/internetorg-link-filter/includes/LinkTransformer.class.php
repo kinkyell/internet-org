3ebcf9c68a89feca9d82077bfd7064b4c5223cf2
Ben Koren <bkoren@nerdery.com>
2015-10-20 14:53:34 -0500

    Added more structure for URL transformation

--- plugins/internetorg-link-filter/includes/LinkTransformer.class.php
+++ plugins/internetorg-link-filter/includes/LinkTransformer.class.php
@@ -27,13 +27,34 @@ class LinkTransformer {
   /**
    * Transforms the provided link to point to the proper domain and language.
    *
-   * @param string $url   The URL to be transformed
+   * @param string $url   The URL to be transformed. May or may not already contain a language code.
    * @return string
    */
   public function transform($url) {
 
-    // @TODO: Transform the URL based on the given language and current domain
+    // We only transform internal URLs, not external (ex: google.com).
+    if (!$this->isInternal($url)) {
+      return $url;
+    }
+
+    $parsedUrl = parse_url($url);
+
+    
+
+    // @TODO: Transform the URL based on the given language and current domain. http://domain/langCode/path
 
     return $url;
+  }
+
+  /**
+   * Determines whether the given URL is internal. This is used to determine whether the link
+   * should be transformed/localized.
+   *
+   * @param string $url
+   * @return bool
+   */
+  protected function isInternal($url) {
+    // @TODO: confirm that this works as intended
+    return internetorg_is_internal_url($url);
   }
 }
