2d56ec6190957458b5d88afef9deadec774312a1
Richard Aber <raber@nerdery.com>
2015-10-21 18:29:38 -0500

    formatting updates to babble-fieldmanager-context to address vip scanner warnings

--- inc/babble-fieldmanager-context.php
+++ inc/babble-fieldmanager-context.php
@@ -1,33 +1,36 @@
 <?php
 /**
  * Babble Fieldmanager Context Class.
+ *
+ * @package Internet.org
  */
 
 /**
  * Class Babble_Fieldmanager_Context
+ *
+ * Extends the Fieldmanager_Context_Storable class for Babble to use.
  */
 class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 
 	/**
-	 * @var string
-	 * Title of meta box
+	 * Title of meta box.
+	 *
+	 * @var string $title
 	 */
 	public $title = '';
 
 	/**
-	 * @var Fieldmanager_Group
-	 * Base field
+	 * Base field.
+	 *
+	 * @var Fieldmanager_Group $fm
 	 */
 	public $fm = null;
 
 	/**
-	 * Add a context to a fieldmanager
+	 * Add a context to a Fieldmanager.
 	 *
-	 * @param string             $title
-	 * @param string|string[]    $post_types
-	 * @param string             $context  (normal, advanced, or side)
-	 * @param string             $priority (high, core, default, or low)
-	 * @param Fieldmanager_Field $fm
+	 * @param string             $title Title.
+	 * @param Fieldmanager_Field $fm    Fieldmanager field.
 	 */
 	public function __construct( $title, $fm = null ) {
 
@@ -36,6 +39,13 @@ class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 
 	}
 
+	/**
+	 * Renders fields.
+	 *
+	 * @param WP_Post   $post   Post.
+	 * @param array     $values Values.
+	 * @param bool|true $echo   Echo.
+	 */
 	public function render_fields( $post, $values = array(), $echo = true ) {
 		$this->fm->data_type = 'post';
 		$this->fm->data_id   = $post->ID;
@@ -47,23 +57,38 @@ class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 		$fm_validation->add_field( $this->fm );
 	}
 
-
+	/**
+	 * Renders a single field.
+	 *
+	 * @param array $args Args.
+	 *
+	 * @return string
+	 */
 	protected function render_field( $args = array() ) {
 		$data = array_key_exists( 'data', $args ) ? $args['data'] : null;
 		$echo = isset( $args['echo'] ) ? $args['echo'] : true;
 
 		$field = $this->fm->element_markup( $data );
+
 		if ( $echo ) {
 			echo $field;
-		} else {
-			return $field;
+
+			return;
 		}
+
+		return $field;
 	}
 
 	/**
 	 * Get post meta.
 	 *
 	 * @see get_post_meta().
+	 *
+	 * @param int        $post_id  Post ID.
+	 * @param string     $meta_key Meta Key.
+	 * @param bool|false $single   Single.
+	 *
+	 * @return mixed
 	 */
 	protected function get_data( $post_id, $meta_key, $single = false ) {
 		return get_post_meta( $post_id, $meta_key, $single );
@@ -73,6 +98,13 @@ class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 	 * Add post meta.
 	 *
 	 * @see add_post_meta().
+	 *
+	 * @param int        $post_id    Post ID.
+	 * @param string     $meta_key   Meta Key.
+	 * @param mixed      $meta_value Meta Value.
+	 * @param bool|false $unique     Unique.
+	 *
+	 * @return void
 	 */
 	protected function add_data( $post_id, $meta_key, $meta_value, $unique = false ) {
 		return;
@@ -82,6 +114,13 @@ class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 	 * Update post meta.
 	 *
 	 * @see update_post_meta().
+	 *
+	 * @param int    $post_id         Post ID.
+	 * @param string $meta_key        Meta Key.
+	 * @param mixed  $meta_value      Meta Value.
+	 * @param string $data_prev_value Data Prev Value.
+	 *
+	 * @return void
 	 */
 	protected function update_data( $post_id, $meta_key, $meta_value, $data_prev_value = '' ) {
 		return;
@@ -91,14 +130,32 @@ class Babble_Fieldmanager_Context extends Fieldmanager_Context_Storable {
 	 * Delete post meta.
 	 *
 	 * @see delete_post_meta().
+	 *
+	 * @param int    $post_id    Post ID.
+	 * @param string $meta_key   Meta Key.
+	 * @param string $meta_value Meta Value.
+	 *
+	 * @return void
 	 */
 	protected function delete_data( $post_id, $meta_key, $meta_value = '' ) {
 		return;
 	}
-
 }
 
+/**
+ * Class Babble_Fieldmanager_TextField
+ *
+ * A Babble compatible version of Fieldmanager_TextField.
+ */
 class Babble_Fieldmanager_TextField extends Fieldmanager_TextField {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -106,7 +163,20 @@ class Babble_Fieldmanager_TextField extends Fieldmanager_TextField {
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_TextArea
+ *
+ * A Babble compatible version of Fieldmanager_TextArea.
+ */
 class Babble_Fieldmanager_TextArea extends Fieldmanager_TextArea {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -114,7 +184,20 @@ class Babble_Fieldmanager_TextArea extends Fieldmanager_TextArea {
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_Media
+ *
+ * A Babble compatible version of Fieldmanager_Media.
+ */
 class Babble_Fieldmanager_Media extends Fieldmanager_Media {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -122,7 +205,20 @@ class Babble_Fieldmanager_Media extends Fieldmanager_Media {
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_Group
+ *
+ * A Babble compatible version of Fieldmanager_Group.
+ */
 class Babble_Fieldmanager_Group extends Fieldmanager_Group {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -130,18 +226,41 @@ class Babble_Fieldmanager_Group extends Fieldmanager_Group {
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_RichTextarea
+ *
+ * A Babble compatible version of Fieldmanager_RichTextarea.
+ */
 class Babble_Fieldmanager_RichTextarea extends Fieldmanager_RichTextarea {
 
+	/**
+	 * A babble element id.
+	 *
+	 * @var null|mixed
+	 */
 	public $babble_element_id = null;
 
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
-		return $context->render_fields( $post, $value, true );;
+		return $context->render_fields( $post, $value, true );
 	}
 
+	/**
+	 * Get a element id.
+	 *
+	 * @return mixed|null
+	 */
 	public function get_element_id() {
-		//allows different id for Babble edit screen
+		// Allows different id for Babble edit screen.
 		if ( true == isset( $this->babble_element_id ) && false === empty( $this->babble_element_id ) ) {
 			return $this->babble_element_id;
 		}
@@ -153,12 +272,25 @@ class Babble_Fieldmanager_RichTextarea extends Fieldmanager_RichTextarea {
 			$el = $el->parent;
 		}
 
-		//wp_editor can not take ID containintg '[]'
+		// Wp_editor can not take ID containintg '[]'.
 		return str_replace( array( '[', ']' ), '-', 'fm-' . implode( '-', $id_slugs ) );
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_Autocomplete
+ *
+ * A Babble compatible version of Fieldmanager_Autocomplete.
+ */
 class Babble_Fieldmanager_Autocomplete extends Fieldmanager_Autocomplete {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -166,7 +298,20 @@ class Babble_Fieldmanager_Autocomplete extends Fieldmanager_Autocomplete {
 	}
 }
 
+/**
+ * Class Babble_Fieldmanager_Link
+ *
+ * A Babble compatible version of Fieldmanager_Link.
+ */
 class Babble_Fieldmanager_Link extends Fieldmanager_Link {
+	/**
+	 * Render a field.
+	 *
+	 * @param string  $name  Name.
+	 * @param mixed   $value Value.
+	 * @param string  $title Title.
+	 * @param WP_Post $post  Post.
+	 */
 	public function render_field( $name, $value, $title, $post ) {
 		$context = new Babble_Fieldmanager_Context( $title, $this );
 
@@ -186,14 +331,42 @@ class Babble_Fieldmanager_Link extends Fieldmanager_Link {
  */
 class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 
+	/**
+	 * Base field.
+	 *
+	 * @var Fieldmanager_Field $fm
+	 */
 	public $fm;
 
+	/**
+	 * Name.
+	 *
+	 * @var string
+	 */
 	public $name;
 
+	/**
+	 * Args.
+	 *
+	 * @var array
+	 */
 	public $args;
 
+	/**
+	 * Translation meta key.
+	 *
+	 * @var string
+	 */
 	public $translation_meta_key = 'bbl_translation[meta]';
 
+	/**
+	 * Constructor.
+	 *
+	 * @param WP_Post $post       Post.
+	 * @param string  $meta_key   Meta Key.
+	 * @param string  $meta_title Meta Title.
+	 * @param array   $args       Args.
+	 */
 	public function __construct( WP_Post $post, $meta_key, $meta_title, array $args = array() ) {
 
 		$this->post       = $post;
@@ -207,7 +380,7 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 
 		$type = "Babble_{$type}";
 
-		//rename
+		// Rename.
 		$this->name      = "{$this->translation_meta_key}[{$meta_key}]";
 		$fm_args['name'] = $this->name;
 
@@ -215,6 +388,12 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 
 	}
 
+	/**
+	 * Get input.
+	 *
+	 * @param string $name  Name.
+	 * @param mixed  $value Value.
+	 */
 	public function get_input( $name, $value ) {
 		add_filter( 'wp_editor_settings', array( $this, 'wp_editor_css_styles' ) );
 		ob_start();
@@ -222,25 +401,30 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 		$field = ob_get_clean();
 		remove_filter( 'wp_editor_settings', array( $this, 'wp_editor_css_styles' ) );
 
-		//escape related-element for use in jQuery
+		// Escape related-element for use in jQuery.
 		$data_related_element_old = sprintf( 'data-related-element="%s"', esc_attr( $this->name ) );
-		$escaped_name = str_replace( '[', '\\[', $this->name );
-		$escaped_name = str_replace( ']', '\\]', $escaped_name );
+		$escaped_name             = str_replace( '[', '\\[', $this->name );
+		$escaped_name             = str_replace( ']', '\\]', $escaped_name );
 		$data_related_element_new = sprintf( 'data-related-element="%s"', $escaped_name );
-		$field = str_replace( $data_related_element_old, $data_related_element_new, $field );
+		$field                    = str_replace( $data_related_element_old, $data_related_element_new, $field );
 
-		//Adjust array position for bbl_translation[meta][home-content-section] format
-		$field = preg_replace_callback( '/data\-fm\-array\-position\=\"(\d+)\"/', function( $matches ) {
+		// Adjust array position for bbl_translation[meta][home-content-section] format.
+		$field = preg_replace_callback( '/data\-fm\-array\-position\=\"(\d+)\"/', function ( $matches ) {
 			$pos = intval( $matches[1] );
 			if ( 0 !== $pos ) {
 				$pos += 2;
 			}
+
 			return sprintf( 'data-fm-array-position="%d"', $pos );
 		}, $field );
 
-		echo $field; //@todo might want to echo
+		// @todo might want to echo.
+		echo $field;
 	}
 
+	/**
+	 * Gets output.
+	 */
 	public function get_output() {
 		$this->set_readonly_attribute();
 		$this->maybe_update_ids();
@@ -260,36 +444,62 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 		$original_meta = 'bbl_translation_original[meta]';
 		$field         = str_replace(
 			sprintf( 'name="%s', $this->translation_meta_key ),
-		    sprintf( 'name="%s', $original_meta ),
-		    $field
+			sprintf( 'name="%s', $original_meta ),
+			$field
 		);
 
-		//echoing the field instead of properly returing it will preserve HTML
+		// Echoing the field instead of properly returing it will preserve HTML.
 		echo $field;
 	}
 
+	/**
+	 * Set readonly attribute on TinyMCE.
+	 *
+	 * @param array $args Args.
+	 *
+	 * @return mixed
+	 */
 	public function readonly_for_tinymce( $args ) {
 		$args['readonly'] = 1;
 
 		return $args;
 	}
 
+	/**
+	 * Set readonly attribute on textarea.
+	 *
+	 * @param string $the_editor The Editor.
+	 *
+	 * @return mixed
+	 */
 	public function readonly_for_editor_textarea( $the_editor ) {
 		$the_editor = str_replace( '>%s</textarea></div>', ' readonly="readonly">%s</textarea></div>', $the_editor );
 
 		return $the_editor;
 	}
 
+	/**
+	 * Add styles to WP_Editor.
+	 *
+	 * @param array $settings Settings.
+	 *
+	 * @return mixed
+	 */
 	public function wp_editor_css_styles( $settings ) {
-		//in case it does not exists
+		// In case it does not exists.
 		if ( false === array_key_exists( 'editor_css', $settings ) ) {
 			$settings['editor_css'] = '';
 		}
 		$settings['editor_css'] .= '<style type="text/css">.fm-richtext textarea {color: #333 !important}</style>';
+
 		return $settings;
 	}
 
-
+	/**
+	 * Maybe update IDs.
+	 *
+	 * @param null $el Element.
+	 */
 	public function maybe_update_ids( $el = null ) {
 		if ( null === $el ) {
 			$el = $this->fm;
@@ -304,6 +514,11 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 		}
 	}
 
+	/**
+	 * Set readonly attribute.
+	 *
+	 * @param null $el Element.
+	 */
 	public function set_readonly_attribute( $el = null ) {
 		if ( null === $el ) {
 			$el = $this->fm;
@@ -316,18 +531,41 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
 		}
 	}
 
+	/**
+	 * Get title.
+	 *
+	 * @return mixed
+	 */
 	public function get_title() {
 		return $this->meta_title;
 	}
 
+	/**
+	 * Get value.
+	 *
+	 * @return mixed
+	 */
 	public function get_value() {
 		return $this->meta_value;
 	}
 
+	/**
+	 * Get key.
+	 *
+	 * @return mixed
+	 */
 	public function get_key() {
 		return $this->meta_key;
 	}
 
+	/**
+	 * Update.
+	 *
+	 * @param mixed   $value Value.
+	 * @param WP_Post $job   Job.
+	 *
+	 * @return mixed
+	 */
 	public function update( $value, WP_Post $job ) {
 		return $value;
 	}
@@ -343,14 +581,41 @@ class Babble_Fieldmanager_Meta_Field extends Babble_Meta_Field {
  */
 class Babble_Translatable_Fieldmanager {
 
+	/**
+	 * Type.
+	 *
+	 * @var null
+	 */
 	public $type = null;
 
+	/**
+	 * Args.
+	 *
+	 * @var array
+	 */
 	public $args = array();
 
+	/**
+	 * Action args.
+	 *
+	 * @var array
+	 */
 	public $action_args = array();
 
+	/**
+	 * Babble meta.
+	 *
+	 * @var null
+	 */
 	public $babble_meta = null;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param string $type    Type.
+	 * @param array  $args    Args.
+	 * @param array  $actions Actions.
+	 */
 	public function __construct( $type, $args, $actions ) {
 
 		$this->type    = $type;
@@ -373,9 +638,24 @@ class Babble_Translatable_Fieldmanager {
 			}
 		}
 
-		//Some fields need to register their scripts early
+		// Some fields need to register their scripts early.
 		if ( 'Fieldmanager_Autocomplete' === $this->type ) {
-			fm_add_script( 'fm_autocomplete_js', 'js/fieldmanager-autocomplete.js', array( 'fieldmanager_script', 'jquery-ui-autocomplete' ), '1.0.5', false, 'fm_search', array( 'nonce' => wp_create_nonce( 'fm_search_nonce' ) ) );
+			fm_add_script(
+				'fm_autocomplete_js',
+				'js/fieldmanager-autocomplete.js',
+				array(
+					'fieldmanager_script',
+					'jquery-ui-autocomplete',
+				),
+				'1.0.5',
+				false,
+				'fm_search',
+				array(
+					'nonce' => wp_create_nonce(
+						'fm_search_nonce'
+					),
+				)
+			);
 		}
 
 		add_filter( 'bbl_translated_meta_fields', array( $this, 'translated_meta_fields' ), 10, 2 );
@@ -383,6 +663,9 @@ class Babble_Translatable_Fieldmanager {
 		add_filter( 'bbl_meta_before_save', array( $this, 'before_meta_save' ), 10, 5 );
 	}
 
+	/**
+	 * Register Fieldmanager meta.
+	 */
 	public function register_fieldmanager_meta() {
 
 		$fm = new $this->type( $this->args );
@@ -407,24 +690,45 @@ class Babble_Translatable_Fieldmanager {
 		}
 	}
 
+	/**
+	 * Translated meta fields.
+	 *
+	 * @param array   $fields Fields.
+	 * @param WP_Post $post   Post.
+	 *
+	 * @return array
+	 */
 	public function translated_meta_fields( array $fields, WP_Post $post ) {
 
 		$name  = $this->args['name'];
 		$title = $this->actions['add_meta_box'][0];
 
-		$this->babble_meta = new Babble_Fieldmanager_Meta_Field( $post, $name, $title, array(
-			'fm' => array(
-				'type' => $this->type,
-				'args' => $this->args,
+		$this->babble_meta = new Babble_Fieldmanager_Meta_Field(
+			$post,
+			$name,
+			$title,
+			array(
+				'fm' => array(
+					'type' => $this->type,
+					'args' => $this->args,
+				),
 			)
-		) );
+		);
 
 		$fields[ $name ] = $this->babble_meta;
 
-		//This is a filter! Don't forget to return $fields
+		// This is a filter! Don't forget to return $fields.
 		return $fields;
 	}
 
+	/**
+	 * Do not sync.
+	 *
+	 * @param bool   $synchronize Synchronize.
+	 * @param string $meta_key    Meta Key.
+	 *
+	 * @return bool
+	 */
 	public function do_not_sync( $synchronize, $meta_key ) {
 		if ( $meta_key === $this->args['name'] ) {
 			$synchronize = false;
@@ -433,8 +737,18 @@ class Babble_Translatable_Fieldmanager {
 		return $synchronize;
 	}
 
+	/**
+	 * Before meta save.
+	 *
+	 * @param mixed   $value      Value.
+	 * @param WP_Post $job        Job.
+	 * @param string  $meta_key   Meta Key.
+	 * @param string  $meta_field Meta Field.
+	 * @param mixed   $meta_data  Meta Data.
+	 *
+	 * @return mixed
+	 */
 	public function before_meta_save( $value, $job, $meta_key, $meta_field, $meta_data ) {
 		return $this->babble_meta->update( $value, $job );
 	}
-
 }
