66584e9faf7b385839b1472afb1c94246b0733a8
Richard Aber <raber@nerdery.com>
2015-10-21 03:54:31 -0500

    Massive overhaul to remove duplicate content that was being created on run.

--- i18n/post-import-update.php
+++ i18n/post-import-update.php
@@ -3,14 +3,11 @@
  * Utility script to assist with importing translated content.
  *
  * PREPARATION:
- * 0. Use the feature/babble-fieldmanager-integration-test branch.
  * 1. Ensure you have all the language packs from production installed in wp-content/languages/.
  * 2. Ensure you have activated all the languages in Babble's Available Languages setting page.
  * 3. Make sure you've set the correct post type for each <item> in the XML (page_fr for example) before import.
  * 4. Import the translated content XML files with WordPress Importer in WP-Admin.
  * 5. Run this script.
- *
- * @todo not a whole lot of empty or error checking going on here, might want to work on that, or maybe doesn't matter.
  */
 
 /**
@@ -62,13 +59,6 @@ foreach ( $post_types as $processing_post_type ) {
 	$bbl_jobs_args = array(
 		'post_type' => $processing_post_type,
 	);
-
-	/**
-	 * An array of bbl_job post_ids keyed by original English post_id.
-	 *
-	 * @var array $babble_job_ids
-	 */
-	$babble_job_ids = [ ];
 
 	/**
 	 * Create a new query.
@@ -130,115 +120,65 @@ foreach ( $post_types as $processing_post_type ) {
 		 */
 		$jobs = $babble_jobs->create_post_jobs( $post->ID, $lang_codes );
 
-		$babble_job_ids[ $post->ID ] = $jobs;
-	}
-
-	/**
-	 * Now we need to associate the posttype_languagecode post type from the XML with the corresponding post_transid_
-	 * taxonomy that was generated when we created the bbl_job posts.
-	 *
-	 * Get the correct post_transid_ from the original Engrish version of the post. The XML that was supplied back to us
-	 * contains the original guid, we can get the original Engrish post_id from that guid.
-	 *
-	 * We also need to serialize data from the posttype_languagecode for use as meta on the bbl_job, as we saw in testing,
-	 * the bbl_job meta is what is actually editable in the Babble translation UI.
-	 *
-	 * For example, the bbl_post_15 meta_key that we examined for a bbl_job translation of original English post 15, had the
-	 * following data serialized in post_meta :
-	 *
-	 *     bbl_post_15 = Array (
-	 *         [post_title] => Our Impact French
-	 *         [post_name] => impact
-	 *         [post_content] => French our impact content
-	 *         [ID] => 0
-	 *         [filter] => db
-	 *     )
-	 *
-	 */
-	foreach ( $lang_codes as $lang_code => $full_code ) {
 		/**
-		 * For testing, only use 'fr' for easier to parse results.
-		 */
-		if ( ! empty( $debug_script ) && 'fr' !== $lang_code ) {
-			continue;
-		}
-
-		/**
-		 * The posttype_languagecode post_type that we are operating on, page_fr for example.
+		 * The post_id of the original English version of this posttype_languagecode post.
 		 *
-		 * @var array $lang_args
+		 * @var int $original_post_id
 		 */
-		$lang_args = array(
-			'post_type' => $processing_post_type . '_' . $lang_code,
-		);
+		$original_post_id = $post->ID;
 
 		/**
-		 * Create a new query for us to work with.
+		 * Get the post_translation term assigned to the original English post. We need that to associate our translated
+		 * version with the original.
 		 *
-		 * @var WP_Query $lang_query
+		 * @var WP_Term[] $post_translation_term
 		 */
-		$lang_query = new WP_Query( $lang_args );
+		$post_translation_terms = wp_get_post_terms( $original_post_id, 'post_translation' );
 
-		if ( ! $lang_query->have_posts() ) {
-			continue;
-		}
+		/**
+		 * Array of term_ids for the post_translation taxonomy that was assigned to the original English post.
+		 *
+		 * @var array $post_translation_term_ids
+		 */
+		$post_translation_term_ids = wp_list_pluck( $post_translation_terms, 'term_id' );
 
-		while ( $lang_query->have_posts() ) {
+		/**
+		 * An array of WP_Post objects of post_type bbl_job that correspond to the original post_id for the
+		 * currently processing language code.
+		 *
+		 * @var WP_Post[] $objects
+		 */
+		$bbl_job_objects = internetorg_get_object_jobs( $original_post_id, 'post', $processing_post_type );
 
-			/** Sets up $post object for loop. */
-			$lang_query->the_post();
+		/**
+		 * Now we need to associate the posttype_languagecode post type from the XML with the corresponding post_transid_
+		 * taxonomy that was generated when we created the bbl_job posts.
+		 *
+		 * Get the correct post_transid_ from the original Engrish version of the post. The XML that was supplied back to us
+		 * contains the original guid, we can get the original Engrish post_id from that guid.
+		 *
+		 * We also need to serialize data from the posttype_languagecode for use as meta on the bbl_job, as we saw in testing,
+		 * the bbl_job meta is what is actually editable in the Babble translation UI.
+		 *
+		 * For example, the bbl_post_15 meta_key that we examined for a bbl_job translation of original English post 15, had the
+		 * following data serialized in post_meta :
+		 *
+		 *     bbl_post_15 = Array (
+		 *         [post_title] => Our Impact French
+		 *         [post_name] => impact
+		 *         [post_content] => French our impact content
+		 *         [ID] => 0
+		 *         [filter] => db
+		 *     )
+		 *
+		 */
 
-			/**
-			 * The files we get back from translators still have the original guid after import, we can extract the original
-			 * post_id from that guid field.
-			 */
-			$match_res = preg_match( '/.*?(\d+)$/', $post->guid, $matches );
+		foreach ( $lang_codes as $lang_code => $full_lang_code ) {
 
-			if ( 1 !== $match_res ) {
+			/** Skip English. */
+			if ( 'en' === $lang_code ) {
 				continue;
 			}
-
-			/**
-			 * The post_id of the original English version of this posttype_languagecode post.
-			 *
-			 * @var int $original_post_id
-			 */
-			$original_post_id = $matches[1];
-
-			/**
-			 * The bbl_job post_ids that were created for the $original_post_id, we'll need to figure out the correct language based on taxonomy... see wp_terms
-			 */
-			$bbl_job_post_ids = $babble_job_ids[ $original_post_id ];
-
-			/**
-			 * Get the post_translation term assigned to the original English post. We need that to associate our translated
-			 * version with the original.
-			 *
-			 * @var WP_Term[] $post_translation_term
-			 */
-			$post_translation_terms = wp_get_post_terms( $original_post_id, 'post_translation' );
-
-			/**
-			 * Array of term_ids for the post_translation taxonomy that was assigned to the original English post.
-			 *
-			 * @var array $post_translation_term_ids
-			 */
-			$post_translation_term_ids = wp_list_pluck( $post_translation_terms, 'term_id' );
-
-			/**
-			 * Set the post_translation term from the original English version on the current posttype_languagecode post.
-			 *
-			 * @var array|WP_Error|string $set_object_terms
-			 */
-			$set_object_terms = wp_set_object_terms( $post->ID, $post_translation_term_ids, 'post_translation' );
-
-			/**
-			 * An array of WP_Post objects of post_type bbl_job that correspond to the original post_id for the
-			 * currently processing language code.
-			 *
-			 * @var WP_Post[] $objects
-			 */
-			$bbl_job_objects = $babble_jobs->get_object_jobs( $original_post_id, 'post', $processing_post_type );
 
 			/**
 			 * The bbl_job to set meta on.
@@ -246,6 +186,39 @@ foreach ( $post_types as $processing_post_type ) {
 			 * @var WP_Post $bbl_job
 			 */
 			$bbl_job = $bbl_job_objects[ $lang_code ];
+
+			if ( empty( $bbl_job ) ) {
+				continue;
+			}
+
+			/**
+			 * The post_id of the corresponding shadow post type.
+			 *
+			 * @var int $shadow_post_id
+			 */
+			$shadow_post_id = absint( internetorg_get_post_id_sql( $post->post_name, $post->post_type . '_' . $lang_code ) );
+
+			if ( empty( $shadow_post_id ) ) {
+				continue;
+			}
+
+			/**
+			 * The corresponding shadow post type.
+			 *
+			 * @var WP_Post $shadow_post
+			 */
+			$shadow_post = get_post( $shadow_post_id );
+
+			if ( empty( $shadow_post ) ) {
+				continue;
+			}
+
+			/**
+			 * Set the post_translation term from the original English version on the current posttype_languagecode post.
+			 *
+			 * @var array|WP_Error|string $set_object_terms
+			 */
+			$set_object_terms = wp_set_object_terms( $shadow_post->ID, $post_translation_term_ids, 'post_translation' );
 
 			/**
 			 * Translated post details for storing in the meta of the bbl_job.
@@ -257,10 +230,10 @@ foreach ( $post_types as $processing_post_type ) {
 			 * @var array $bbl_post_meta_value
 			 */
 			$bbl_post_meta_value = array(
-				'post_title'   => $post->post_title,
-				'post_name'    => $post->post_name,
-				'post_content' => $post->post_content,
-				'id'           => $post->ID,
+				'post_title'   => $shadow_post->post_title,
+				'post_name'    => $shadow_post->post_name,
+				'post_content' => $shadow_post->post_content,
+				'id'           => $shadow_post->ID,
 				'filter'       => 'db',
 			);
 
@@ -302,4 +275,79 @@ foreach ( $post_types as $processing_post_type ) {
 			wp_update_post( $post_status_update );
 		}
 	}
+}
+
+/**
+ * Get a post_id by post_name and post_type with SQL.
+ *
+ * @param $post_name The post_name of the post. Required.
+ * @param $post_type The post_type of the post. Required.
+ *
+ * @global wpdb $wpdb The global WordPress database object. Required.
+ *
+ * @return null|string
+ */
+function internetorg_get_post_id_sql( $post_name, $post_type ) {
+	global $wpdb;
+
+	$post_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_type = %s", $post_name, $post_type ) );
+
+	return $post_id;
+}
+
+/**
+ * Get Babble jobs for a given original post.
+ *
+ * @param int $id The original object id.
+ * @param string $type The type, either post or term.
+ * @param string $post_type The original post_type.
+ *
+ * @return WP_Post[] An array of WP_Post objects of type bbl_job corresponding to the original object id.
+ */
+function internetorg_get_object_jobs( $id, $type, $post_type ) {
+
+	$jobs = get_posts(
+		array(
+			'bbl_translate'  => false,
+			'post_type'      => 'bbl_job',
+			'post_status'    => 'any',
+			'meta_key'       => "bbl_job_{$type}",
+			'meta_value'     => "{$post_type}|{$id}",
+			'posts_per_page' => - 1,
+			'post_status'    => 'any',
+		)
+	);
+
+	if ( empty( $jobs ) ) {
+		return array();
+	}
+
+	$jobs   = array_map( 'get_post', $jobs );
+
+	$return = array();
+
+	foreach ( $jobs as $job ) {
+		if ( $lang = internetorg_get_job_language( $job ) ) {
+			$return[ $lang->code ] = $job;
+		}
+	}
+
+	return $return;
+}
+
+/**
+ * Get the language of a bbl_job.
+ *
+ * @param $job
+ *
+ * @return bool|object
+ */
+function internetorg_get_job_language( $job ) {
+	$job       = get_post( $job );
+	$languages = get_the_terms( $job, 'bbl_job_language' );
+	if ( empty( $languages ) ) {
+		return false;
+	}
+
+	return bbl_get_lang( reset( $languages )->name );
 }
