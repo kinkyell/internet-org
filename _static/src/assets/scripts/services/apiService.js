1cb10e4df29a15614bb2f8c9323d49fb4d21b619
Nick Stark <nstark@nerdery.com>
2015-10-21 15:19:55 -0500

    updated translations in jst for read more text

--- _static/src/assets/scripts/services/apiService.js
+++ _static/src/assets/scripts/services/apiService.js
@@ -122,7 +122,8 @@ define(function(require, exports, module) { // jshint ignore:line
                             image: result.panel_image,
                             mobileImage: result.mobile_image,
                             isPost: result.post_type === 'post',
-                            isStory: result.post_type === 'io_story'
+                            isStory: result.post_type === 'io_story',
+                            readMoreText: window.SETTINGS.READ_MORE_TEXT
                         });
                     }).join('')
                 };
@@ -168,7 +169,8 @@ define(function(require, exports, module) { // jshint ignore:line
                             desc: result.post_excerpt,
                             date: result.post_date,
                             url: result.permalink,
-                            image: result.post_thumbnail
+                            image: result.post_thumbnail,
+                            readMoreText: window.SETTINGS.READ_MORE_TEXT
                         });
                     }).join('')
                 };
