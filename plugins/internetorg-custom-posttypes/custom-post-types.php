2e0b37c4180f762d54efc68f821814728d6e0bdd
Richard Aber <raber@nerdery.com>
2015-10-21 15:55:28 -0500

    remove free services and campaign post types

--- plugins/internetorg-custom-posttypes/custom-post-types.php
+++ plugins/internetorg-custom-posttypes/custom-post-types.php
@@ -46,14 +46,10 @@ class Internetorg_CustomPostTypes {
 	 * Create post types.
 	 */
 	public function create_post_types() {
-		$internetorg_cpt_freeService = new Internetorg_FreeService_PostType();
-		$internetorg_cpt_campaign    = new Internetorg_Campaign_PostType();
 		$internetorg_cpt_story       = new Internetorg_Story_PostType();
 		$internetorg_cpt_widget      = new Internetorg_ContentWidget_PostType();
 		$internetorg_cpt_video      = new Internetorg_Video_PostType();
 
-		$internetorg_cpt_freeService->register();
-		$internetorg_cpt_campaign->register();
 		$internetorg_cpt_story->register();
 		$internetorg_cpt_widget->register();
 		$internetorg_cpt_video->register();
