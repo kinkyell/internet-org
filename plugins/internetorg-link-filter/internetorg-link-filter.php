d551645d2dc3e63476c8273855ec458aa3246eb7
Ben Koren <bkoren@nerdery.com>
2015-10-20 20:51:47 -0500

    Bound LinkTransformer to WP for easier usage in WP filters

--- plugins/internetorg-link-filter/internetorg-link-filter.php
+++ plugins/internetorg-link-filter/internetorg-link-filter.php
@@ -9,11 +9,11 @@
 require_once dirname(__FILE__) . '/includes/ContentParser.class.php';
 require_once dirname(__FILE__) . '/includes/LinkTransformer.class.php';
 
-$current_site = get_current_site();
-$transformer = new LinkTransformer($current_site->domain);
+$transformer = new LinkTransformer();
 $parser = new ContentParser($transformer);
 
 add_filter('the_content', array($parser, 'parseLinks'), 100);
 add_filter('widget_content', array($parser, 'parseLinks'), 100);
 add_filter('the_excerpt', array($parser, 'parseLinks'), 100);
 add_filter('get_post_metadata', array($parser, 'parseLinks'), 100);
+add_filter('the_permalink', array($transformer, 'transform'), 100);
