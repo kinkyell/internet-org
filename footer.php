6325cf989ea1553d1d3e8502267bb99965983057
Jonathan McKinnell <jonathan@jam3.com>
2015-11-11 11:09:36 -0500

    Fixing footer issue

--- footer.php
+++ footer.php
@@ -17,16 +17,7 @@
 
 	<?php /* <div><?php echo vip_powered_wpcom(); ?></div> */ ?>
 
-	<script type="text/javascript">
-    		window._define = window.define;
-    		window.define = null;
-	</script>
-
 	<?php wp_footer(); /* required */ ?>
-
-	<script type="text/javascript">
-    		window.define = window._define;
-	</script>
 
 	<?php get_template_part( 'template-parts/footer', 'requirejs' ); ?>
 
