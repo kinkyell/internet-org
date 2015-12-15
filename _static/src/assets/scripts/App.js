c6956ffc26e49f16bc0ac4613cdd0ac5fbc16e55
andrewdyke <el.dyko@gmail.com>
2015-12-15 11:38:29 -0500

    Added JS to detect Opera Mini and add class to body

--- _static/src/assets/scripts/App.js
+++ _static/src/assets/scripts/App.js
@@ -179,27 +179,24 @@ define(function(require, exports, module) { // jshint ignore:line
 });
 
 
-// Detect Opera Mini take 4
+// Detect Opera Mini
 var isOperaMini = Object.prototype.toString.call(window.operamini) === '[object OperaMini]';
 
+// If Opera Mini add class to body
 if (isOperaMini === true) {
-    alert('so yeah its opera mini');
-    var root = document.getElementByTagName('body')[0];     
+    alert('This website is running in Opera Mini');
+    var root = document.getElementsByTagName('body')[0];     
     root.className += ' opera-mini';
 }
 
-// if browser is IE10 attempt 2
+// // Add Class Test
+// var root = document.getElementsByTagName('body')[0];     
+// root.className += ' opera-mini';
+
+
+// if browser is IE10
 var doc = document.documentElement;
 doc.setAttribute('data-useragent', navigator.userAgent);
-
-
-
-
-
-
-
-
-
 
 
 
