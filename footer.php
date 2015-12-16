9f6d66ccbae6c4e064bf6354bc04e6c97c2507cf
andrewdyke <el.dyko@gmail.com>
2015-12-16 18:38:47 -0500

    Added support for Opera Mini

--- footer.php
+++ footer.php
@@ -10,9 +10,29 @@
 ?>
 	<!-- Add asset shade BEFORE scripts are loaded -->
 	<script type="text/javascript">
+
 		var loader = document.createElement('div');
 		loader.className = 'loadingIcon loadingIcon_opaque loadingIcon_topLayer js-assetShade';
 		document.body.appendChild(loader);
+
+		// Detect Opera Mini
+		var isOperaMini = Object.prototype.toString.call(window.operamini) === '[object OperaMini]';
+
+	// Ext Execution of JavaScript
+		function exit(e){function o(e){e.stopPropagation()}var t;window.addEventListener("error",function(e){e.preventDefault(),e.stopPropagation()},!1);var n=["copy","cut","paste","beforeunload","blur","change","click","contextmenu","dblclick","focus","keydown","keypress","keyup","mousedown","mousemove","mouseout","mouseover","mouseup","resize","scroll","DOMNodeInserted","DOMNodeRemoved","DOMNodeRemovedFromDocument","DOMNodeInsertedIntoDocument","DOMAttrModified","DOMCharacterDataModified","DOMElementNameChanged","DOMAttributeNameChanged","DOMActivate","DOMFocusIn","DOMFocusOut","online","offline","textInput","abort","close","dragdrop","load","paint","reset","select","submit","unload"];for(t=0;t<n.length;t++)window.addEventListener(n[t],function(e){o(e)},!0);throw window.stop&&window.stop(),""};
+
+		// If Opera Mini add class to body
+		if (isOperaMini === true) {
+			var root = document.getElementsByTagName('body')[0];     
+			root.className += ' opera-mini';
+			exit( 'Opera Mini Detected' );
+		}
+
+		// remove this later..its just for testing
+		var root = document.getElementsByTagName('body')[0];     
+		root.className += ' opera-mini';
+		exit( 'Opera Mini Detected' );
+
 	</script>
 
 	<?php /* <div><?php echo vip_powered_wpcom(); ?></div> */ ?>
