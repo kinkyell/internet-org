be8f6d7cee7d0cf87e72030fe6033b7f2aa96572
maximobelen <maximo@jam3.com>
2016-01-12 13:15:38 -0300

    try html5 video

--- page-navigation.php
+++ page-navigation.php
@@ -39,6 +39,11 @@ get_header();
         </ul>
       </div>
     </div>
+      <video width="320" height="240" controls>
+    <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+    <source src="http://www.w3schools.com/html/mov_bbb.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+  </video>
   </div>
 
 <?php endwhile; // End of the loop. ?>
