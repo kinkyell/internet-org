01f9298ea82dc7fff239e04f3d2dbceeb975a458
maximobelen <maximo@jam3.com>
2016-01-14 15:58:07 -0300

    add video page for testing

---
+++ page-video.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the video test page
+ *
+ * Template Name: Video
+ *
+ * @package Internet.org
+ */
+
+get_header();
+
+?>
+
+<video controls="" style="width:640px;height:360px;" poster="http://www.html5rocks.com/en/tutorials/video/basics/poster.png">
+    <source src="http://www.html5rocks.com/en/tutorials/video/basics/devstories.webm" type="video/webm;codecs=&quot;vp8, vorbis&quot;">
+		<a href="http://www.w3schools.com/html/mov_bbb.mp4">Video</a>
+</video>
+
+
+<?php get_footer();
