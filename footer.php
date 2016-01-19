298eb0533a9ab54e787f0c7f05bd4db5ba1b633e
Darcy Clarke <darcy@darcyclarke.me>
2016-01-19 12:10:28 -0500

    Get php information

--- footer.php
+++ footer.php
@@ -19,38 +19,36 @@
 
 		function applyOperaFallbacks () {
 			var root = document.getElementsByTagName('body')[0];
-			if (isOperaMini) {
-				root.className += ' opera-mini';
-				exit( 'Opera Mini Detected' );
-			}
-
-			if (isOpera10) {
-				//root.className += ' opera-10';
-				root.className += ' opera-mini';
-				exit( 'Opera 10 Detected' );
-			}
+			root.className += ' opera-mini';
+			exit( 'Opera Mini Detected' );
 		}
 
-		function IE(v) {
-		  return RegExp('msie' + (!isNaN(v)?('\\s'+v):''), 'i').test(navigator.userAgent);
-		}
+		// function IE(v) {
+		//   return RegExp('msie' + (!isNaN(v)?('\\s'+v):''), 'i').test(navigator.userAgent);
+		// }
 
-		if(IE(10)){
-			var root = document.getElementsByTagName('body')[0];
-			root.className += ' ie10';
-			console.log('Internet Explorer 10');
-		}
+		// if(IE(10)){
+		// 	var root = document.getElementsByTagName('body')[0];
+		// 	root.className += ' ie10';
+		// 	console.log('Internet Explorer 10');
+		// }
 
 		// If Opera Mini or Opera 10 add class to body
-		if ( isOperaMini === true || isOpera10 === true) {
-			applyOperaFallbacks();
-		}
+		// if ( isOperaMini === true || isOpera10 === true) {
+		// 	applyOperaFallbacks();
+		// }
+
+		applyOperaFallbacks();
 
 		var loader = document.createElement('div');
 		loader.className = 'loadingIcon loadingIcon_opaque loadingIcon_topLayer js-assetShade';
 		document.body.appendChild(loader);
 
 	</script>
+
+	<!--
+	<?php phpinfo(); ?>
+	-->
 
 	<div id="fb-root"></div>
   <script>(function(d, s, id) {
