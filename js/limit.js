1dcd8d748192dd9ff82c42facd6d24db754ed7ce
Darcy Clarke <darcy@darcyclarke.me>
2016-01-25 16:12:59 -0500

    Fix up title validation to only limit to 100 characters

--- js/limit.js
+++ js/limit.js
@@ -1,21 +1,26 @@
 
 jQuery( function ( $ ) {
 
-	var setCounter = function ( field ) {
-		var remaining = 300 - field.value.length;
+	var setCounter = function ( field, limit ) {
+		var remaining = limit - field.value.length;
 		var $count = $( field ).siblings( '.counter' );
 		$count.text( remaining + ' characters remaining.');
 	};
 
-	$( '.fm-iorg_description textarea, .fm-iorg_title input' ).each( function () {
-
+	$( '.fm-iorg_description textarea' ).each( function () {
 		$( this ).after( '<div class="counter">300 characters remaining.</div>' );
-		setCounter( this );
-
-	}).on( 'keypress' , function () {
-
-		setCounter( this );
-
+		setCounter( this, 300 );
+	}).on( 'keyup blur', function () {
+		setCounter( this, 300 );
 	});
+
+	$( '.fm-iorg_title input' ).each( function () {
+		$( this ).after( '<div class="counter">100 characters remaining.</div>' );
+		setCounter( this, 100 );
+	}).on( 'keyup blur', function () {
+		setCounter( this, 100 );
+	});
+
+
 
 });
