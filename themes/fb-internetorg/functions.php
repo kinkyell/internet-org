9907077a34aacb9d77f245df9c655a935d3e6342
Jason Agnew <jason@bigbitecreative.com>
2016-04-19 13:15:00 +0100

    Fix for insert link filter.

--- themes/fb-internetorg/functions.php
+++ themes/fb-internetorg/functions.php
@@ -2704,11 +2704,17 @@ function internetorg_strip_domain_from_insert_link( $permalink, $post )
 {
 	$site_url = get_site_url();
 
-	if ( is_ssl() && substr( $site_url, 0, 8 ) != 'https://' ) {
-		$site_url = str_replace( 'http://', 'https://', $site_url );
+	if ( substr( $site_url, 0, 8 ) == 'https://' ) {
+		$site_url_http  = $site_url;
+		$site_url_https = str_replace( 'http://', 'https://', $site_url );
+	} else {
+		$site_url_http  = str_replace( 'https://', 'http://', $site_url );
+		$site_url_https = $site_url;
 	}
 
-	$permalink = str_replace( $site_url, '/en', $permalink );
+	$permalink = str_replace( $site_url_http, '/en', $permalink );
+	$permalink = str_replace( $site_url_https, '/en', $permalink );
+
     return $permalink;
 }
 
