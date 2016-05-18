cd3f74977325aefc317f221f7a4b04bdb29fdf91
Bobby Yang <bobby.yang@bynd.com>
2016-05-18 19:47:07 +0530

    Added new template design

--- plugins/internetorg-custom-fields/internetorg-custom-fields.php
+++ plugins/internetorg-custom-fields/internetorg-custom-fields.php
@@ -33,15 +33,206 @@ if ( ! function_exists( 'internetorg_custom_fields_init' ) ) {
 	 *
 	 * @return void
 	 */
+	
 	function internetorg_custom_fields_init() {
 		/*
 		 * The desire is have these fields only on the home page but we cannot limit these custom fields to a specific
 		 * page upon page creation, we can, however, limit on display with a custom page template for those pages
 		 * on which we wish to display the content.
 		 */
+		//add_action( 'wp_enqueue_scripts', 'register_plugin_styles' );
+		add_action( 'admin_enqueue_scripts', 'register_plugin_styles' );
 		add_action( 'edit_form_after_title', 'internetorg_page_home_after_title_fields' );
-
+		add_action( 'add_meta_boxes', 'my_custom_field_checkboxes' );
+		add_action( 'save_post', 'my_custom_field_data', 1);
+		add_filter( 'the_content_more_link', 'modify_read_more_link' );
+add_filter( 'fee_rich_clean', '__return_false' );
 		return;
+	}
+
+
+	function modify_read_more_link() {
+	return '<a class="more-link" href="' . get_permalink() . '">Your Read More Link Text</a>';
+	}
+
+	function register_plugin_styles() {
+		wp_register_style( 'custom-style', plugins_url('internetorg-custom-fields.css', __FILE__ ), array(), '1', 'all' );
+		wp_enqueue_style( 'custom-style' );
+		wp_register_script( 'custom-script', plugins_url('internetorg-custom-fields.js', __FILE__ ), array(), '1', 'all' );
+		wp_enqueue_script( 'custom-script' );
+	}
+		// register the meta box
+	
+	function my_custom_field_checkboxes() {
+	    add_meta_box(
+	        'my_meta_box_id',          // this is HTML id of the box on edit screen
+	        'Internet org custom fields',    // title of the box
+	        'my_customfield_box_content',   // function to be called to display the checkboxes, see the function below
+	        'post',        // on which edit screen the box should appear
+	        'normal',      // part of page where the box should appear
+	        'default'      // priority of the box
+	    );
+	}
+
+	// display the metabox
+	function my_customfield_box_content( $post ) {
+	    // nonce field for security check, you can have the same
+	    // nonce field for all your meta boxes of same plugin
+	    $post_id = $post->ID;
+	    wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_nonce' );
+
+	  
+
+	    $display_date = get_post_meta( $post_id, 'iorg_display_date') ? get_post_meta( $post_id, 'iorg_display_date')[0] : '';
+	    $show_footer = get_post_meta( $post_id, 'iorg_show_footer') ? get_post_meta( $post_id, 'iorg_show_footer')[0] : '';
+	    $show_hero = get_post_meta( $post_id, 'iorg_show_hero') ? get_post_meta( $post_id, 'iorg_show_hero')[0] : '';
+
+	    $hero_image = get_post_meta( $post_id, 'iorg_hero_image') ? get_post_meta( $post_id, 'iorg_hero_image')[0] : '';
+	    $vdo_url = get_post_meta( $post_id, 'iorg_hero_vdo_url') ? get_post_meta( $post_id, 'iorg_hero_vdo_url')[0] : '';
+	    $header_color = get_post_meta( $post_id, 'iorg_header_color') ? get_post_meta( $post_id, 'iorg_header_color')[0]: '';
+
+	    $header_img_color = get_post_meta( $post_id, 'iorg_header_img_color') ? get_post_meta( $post_id, 'iorg_header_img_color')[0]: '';
+
+	    $story_page = get_post_meta( $post_id, 'iorg_story_page') ? get_post_meta( $post_id, 'iorg_story_page')[0]: '';
+
+	    if($display_date!="N") {
+	    	$show_display_date = " checked ";	
+	    } else {
+	    	$show_display_date = "";
+	    }
+
+	    if($show_footer!="N") {
+	    	$show_show_footer = " checked ";	
+	    } else {
+	    	$show_show_footer = "";
+	    }
+
+	    if($show_hero=="Y") {
+	    	$display_hero = " checked ";
+	    } else {
+	    	$display_hero = "";
+	    }
+
+
+
+
+	    	?>
+	    <div class="iorg-custom-fields">
+	    	<div class="iorg-custom-fields-left">Show  post date</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<input type="checkbox" name="iorg_display_date" value="Y" <?php echo $show_display_date; ?> />
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Show footer more posts</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<input type="checkbox" name="iorg_show_footer" value="Y" <?php echo $show_show_footer; ?> />
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div><h3>Hero Details</h3></div>
+	    	<div class="iorg-custom-fields-left">Show Hero</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<input type="checkbox" name="iorg_show_hero" value="Y" <?php echo $display_hero; ?> />
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Hero Image</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<input type="text" class="iorg_text" id="iorg_hero_image" name="iorg_hero_image" value="<?php echo $hero_image; ?>" />
+	    		<button id="iorg_select_img" onclick="javascript: selectMedia('iorg_select_img', 'iorg_hero_image');">Select Media</button>
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Hero Video URL</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<input type="text" class="iorg_text" id="iorg_hero_vdo_url" name="iorg_hero_vdo_url" value="<?php echo $vdo_url; ?>" />
+	    		</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Header image color</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<select name="iorg_header_img_color">
+			    	<option value="white" <?php if($header_img_color=="white") echo " selected "; ?> >White</option>
+			    	<option value="black" <?php if($header_img_color!="white") echo " selected "; ?>>Black</option>
+		    	</select>
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Header menu color</div>
+	    	<div class="iorg-custom-fields-right">
+	    		<select name="iorg_header_color">
+			    	<option value="white" <?php if($header_color=="white") echo " selected "; ?> >White</option>
+			    	<option value="black" <?php if($header_color!="white") echo " selected "; ?>>Black</option>
+			    	
+		    	</select>
+	    	</div>
+	    	<div class="iorg-custom-fields-clear"></div>
+	    	<div class="iorg-custom-fields-left">Story Page</div>
+	    	<div class="iorg-custom-fields-right">
+		    	<select name="iorg_story_page">
+			    	<option value="half_screen" <?php if($story_page=="half_screen") echo " selected "; ?> >Half Screen</option>
+			    	<option value="full_screen" <?php if($story_page!="half_screen") echo " selected "; ?>>Full Screen</option>
+			    	
+		    	</select>
+		    </div> 
+	    	<div class="iorg-custom-fields-clear"></div>
+	    </div>
+	    
+	    <?php
+
+	}
+
+	// save data from checkboxes
+	
+	function my_custom_field_data() {
+
+		if( !isset($_POST['post_ID'] ) )
+			return false;
+		$post_id = $_POST['post_ID'];
+		
+		// check if this isn't an auto save
+	    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+	        return;
+	    
+		
+	    // further checks if you like, 
+	    // for example particular user, role or maybe post type in case of custom post types
+	    
+	    // now store data in custom fields based on checkboxes selected
+	    if ( isset( $_POST['iorg_display_date'] ) )
+	        update_post_meta( $post_id, 'iorg_display_date', 'Y' );
+	    else
+	        update_post_meta( $post_id, 'iorg_display_date', 'N' );
+
+	    if ( isset( $_POST['iorg_show_footer'] ) )
+	        update_post_meta( $post_id, 'iorg_show_footer', 'Y' );
+	    else
+	        update_post_meta( $post_id, 'iorg_show_footer', 'N' );
+
+	    if ( isset( $_POST['iorg_show_hero'] ) )
+	        update_post_meta( $post_id, 'iorg_show_hero', 'Y' );
+	    else
+	        update_post_meta( $post_id, 'iorg_show_hero', 'N' );
+
+	    if ( isset( $_POST['iorg_hero_image'] ) )
+	        update_post_meta( $post_id, 'iorg_hero_image', $_POST['iorg_hero_image'] );
+	    else
+	        update_post_meta( $post_id, 'iorg_hero_image', '' );
+
+	    if ( isset( $_POST['iorg_hero_vdo_url'] ) )
+	        update_post_meta( $post_id, 'iorg_hero_vdo_url', $_POST['iorg_hero_vdo_url'] );
+	    else
+	        update_post_meta( $post_id, 'iorg_show_footer', '' );
+
+	    if ( isset( $_POST['iorg_header_color'] ) )
+	        update_post_meta( $post_id, 'iorg_header_color', $_POST['iorg_header_color'] );
+	    else
+	        update_post_meta( $post_id, 'iorg_header_color', '' );
+
+	    if ( isset( $_POST['iorg_header_img_color'] ) )
+	        update_post_meta( $post_id, 'iorg_header_img_color', $_POST['iorg_header_img_color'] );
+	    else
+	        update_post_meta( $post_id, 'iorg_header_img_color', '' );
+
+	    if ( isset( $_POST['iorg_story_page'] ) )
+	        update_post_meta( $post_id, 'iorg_story_page', $_POST['iorg_story_page'] );
+	    else
+	        update_post_meta( $post_id, 'iorg_story_page', '' );
 	}
 }
 add_action( 'init', 'internetorg_custom_fields_init' );
--- themes/fb-internetorg/single.php
+++ themes/fb-internetorg/single.php
@@ -7,10 +7,62 @@
 
 get_header();
 
-?>
+ $showDate = get_post_custom_values('iorg_display_date'); 
+ $displayDate = "show";
+ $displayFooterPosts = "show";
 
-	<div class="viewWindow isShifted js-viewWindow js-stateDefault" id="main-content" role="main" data-type="titled" data-route="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>" data-social="true" data-date="<?php internetorg_posted_on_date(); ?>">
-		<?php while ( have_posts() ) : the_post(); ?>
+if(is_array($showDate) && strtolower($showDate[0])=="n") {
+ 	$displayDate = "hide";
+
+ } else {
+ 	$displayDate = "show";
+
+ }
+
+$showMedia = get_post_custom_values('iorg_hero_vdo_url'); 
+$showImage = get_post_custom_values('iorg_hero_image'); 
+$showHero = get_post_custom_values('iorg_show_hero'); 
+if(is_array($showHero) && ($showHero[0]!="Y")) {
+	if(is_array($showImage)) {
+		$showImage[0] = "";
+	}	
+}
+
+$showFooterPosts = get_post_custom_values('iorg_show_footer'); 
+if(is_array($showFooterPosts) && strtolower($showFooterPosts[0])=="n") {
+ 	$displayFooterPosts = "hide";
+
+ } else {
+ 	$displayFooterPosts = "show";
+
+ }
+
+
+
+
+$story_page = get_post_custom_values('iorg_story_page'); 
+$display_story = "full_screen";
+if(is_array($story_page) && $story_page[0]!="") {
+		$display_story = $story_page[0];
+	} 
+$display_margin = "";
+$display_viewwindow_rtl = "";
+
+if($display_story=="half_screen") {
+	$display_margin = 'ContainerPadding';
+
+} else {
+	$display_viewwindow_rtl = "isShiftedRtl";
+}
+
+$header_color = get_post_custom_values('iorg_header_color'); 
+$header_img_color = get_post_custom_values('iorg_header_img_color'); 
+
+
+?>
+	<div class="viewWindow isShifted js-viewWindow js-stateDefault <?php echo $display_viewwindow_rtl; ?>" id="main-content" role="main" data-type="titled" data-route="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>" data-social="true" data-date="<?php if($displayDate=='show') internetorg_posted_on_date(); ?>" <?php if(is_array($showImage) && $showImage[0]!="") { ?> data-image-display="<?php echo $showImage[0]; ?>" <?php } ?> <?php if(is_array($showMedia) && $showMedia[0]!="") { ?> data-video="<?php echo $showMedia[0]; ?>" <?php } ?> data-story-page="<?php echo $display_story; ?>" <?php if(is_array($header_color) && $header_color[0]!="") { ?> data-header-color="<?php echo $header_color[0]; ?>" <?php } ?> <?php if(is_array($header_img_color) && $header_img_color[0]!="") { ?> data-header-img-color="<?php echo $header_img_color[0]; ?>" <?php } ?>>
+		<?php while ( have_posts() ) : the_post(); 
+		 ?>
 
 			<?php get_template_part( 'template-parts/content', 'page-temp-panel' ); ?>
 
@@ -21,14 +73,29 @@ get_header();
 						<div class="introBlock-inner">
 							<div class="container">
 								<div class="topicBlock">
+								<?php
+								if(is_array($showImage) && $showImage[0]!="") {
+								?>
+								<div class="imgWrap isLoaded"  id="heroImage" style="background: url(<?php echo $showImage[0]; ?>) no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: over; background-size: cover; width:100%;">&nbsp;</div>
+								<?php
+								 }
+								?>
 
+								<?php if(is_array($showMedia) && $showMedia[0]!="") { ?>
+								<a href="<?php echo $showMedia[0]; ?>" class="contentOnMedia-link contentOnMedia-link_ct js-videoModal swipebox-video HeroImagePlay">
+									<span class="circleBtn circleBtn_play"></span>
+								</a>
+								<?php }//media ?>
 									<div class="topicBlock-hd topicBlock-hd_plus">
 										<h2 class="hdg hdg_2 mix-hdg_bold"><?php the_title(); ?></h2>
 									</div>
+									<?php 
+										if($displayDate=="show") {
+									?>
 									<div class="topicBlock-subHd">
 										<div class="hdg hdg_6 mix-hdg_italic mix-hdg_gray"><?php internetorg_posted_on_date(); ?></div>
 									</div>
-
+									<?php }  ?>
 								</div>
 							</div>
 						</div>
@@ -44,15 +111,30 @@ get_header();
 					<div>
 						<?php get_template_part( 'template-parts/content', 'press-intro-mobile' ); ?>
 
-						<div class="contentCol">
-							<div class="container">
+						<div class="contentCol_fullPage">
+							<div class="container <?php echo $display_margin; ?>">
 								<div class="feature">
 									<div class="feature-bd wysiwyg quarantine">
 
 										<?php internetorg_media_embed(); ?>
+										<?php 
+										
+										// Fetch post content
+										$content = get_post_field( 'post_content', $post->ID);
 
-										<?php the_content(); ?>
-
+										// Get content parts
+										$content_parts = get_extended( $content );
+										$content_short = apply_filters('the_content', $content_parts['main']);
+										$content_extended = apply_filters('the_content', $content_parts['extended']);
+										
+										echo $content_short; 
+										if($content_extended!="") {
+										 ?>
+										 <div class="readMoreDiv" ><button onclick="javascript:  jQuery('.readMoreDiv').hide(); jQuery('#extendedContent').show('slow');" class="readMore">SHOW MORE</button></div>
+										 <div id="extendedContent" style="display: none;">
+										 	<?php echo $content_extended; ?>
+										 </div>
+										 <?php } ?>
 									  <div class="fb-like"
 									      data-layout="standard"
 									      data-action="like"
@@ -65,9 +147,11 @@ get_header();
 						</div>
 
 						<div class="footBox">
+							<?php if($displayFooterPosts=="show") { ?>
 							<div class="container">
 								<?php get_template_part( 'template-parts/content', 'single-more' ); ?>
 							</div>
+							<?php } ?>
 							<div class="footBox-ft">
 								<div class="container">
 									<?php internetorg_vip_powered_wpcom( 'pwdByVip-txt' ); ?>
